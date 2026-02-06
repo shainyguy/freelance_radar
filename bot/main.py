ef4bfadb9100bbc2d4d99fc258958287a0d91f46
shainyguy <shainyguy@gmail.com>
2026-02-06 15:06:40 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -946,7 +946,7 @@ WEBAPP_HTML = '''<!DOCTYPE html>
     
     <script>
         const API = '{{API_BASE}}';
-        const tg = Telegram.WebApp.openLink(payment_url);
+        const tg = window.Telegram.WebApp.openLink(payment_url);
         
         let user = null;
         let orders = [];
@@ -1235,5 +1235,6 @@ WEBAPP_HTML = '''<!DOCTYPE html>
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
